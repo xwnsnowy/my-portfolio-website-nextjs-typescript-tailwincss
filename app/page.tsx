c18986c50e4c07db3dd1e5bcf297964a62ac39b3
Nguyen Tien Thanh <thanhnthe163528@fpt.edu.vn>
2024-08-27 16:15:05 +0700

    feat: add right section of Home page with animated image and circle

--- app/page.tsx
+++ app/page.tsx
@@ -9,11 +9,11 @@ export default function Home() {
     <section className="h-full">
       <div className="container mx-auto h-full">
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
-          <div className="text-center xl:text-left">
+          <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Software Developer</span>
             <h1 className="h1 mb-6">
               Hello I&apos;m <br />
-              <span className="text-accent">Tien Thanh</span>
+              <span className="text-yellow-200">Tien Thanh</span>
             </h1>
             <p className="max-w-[500px] mb-9 text-white/80">
               I excel at crafting elegant digital experiences an I am proficient
@@ -31,12 +31,12 @@ export default function Home() {
               <div className="mb-8 xl:mb-0">
                 <Socials
                   containerStyles="flex gap-6"
-                  iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accaent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500"
+                  iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500"
                 />
               </div>
             </div>
           </div>
-          <div>
+          <div className="order-1 xl:order-none mb-8 xl:mb=0">
             <Photo />
           </div>
         </div>
--- components/Photo.tsx
+++ components/Photo.tsx
@@ -1,7 +1,62 @@
 "use client";
 
+import { motion } from "framer-motion";
+import Image from "next/image";
+
 const Photo = () => {
-  return <div></div>;
+  return (
+    <div className="h-full w-full relative">
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 1, duration: 0.4, ease: "easeIn" }}
+      >
+        <motion.div
+          initial={{ opacity: 0, scale: 0.8 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ delay: 1.1, duration: 0.7, ease: "easeInOut" }}
+        >
+          <div className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-normal absolute">
+            <Image
+              src="/assets/home-img.png"
+              priority
+              quality={100}
+              fill
+              alt=""
+              className="object-contain"
+            />
+          </div>
+        </motion.div>
+
+        <motion.svg
+          className="w-[300px] xl:w-[506px] h-[300px] xl:h-[506px]"
+          fill="transparent"
+          viewBox="0 0 506 506"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <motion.circle
+            cx="253" //  Đặt tọa độ trung tâm của hình tròn.
+            cy="253" //  Đặt tọa độ trung tâm của hình tròn.
+            r="250" // Đặt bán kính của hình tròn.
+            stroke="#00ff99" // Đặt màu viền của hình tròn.
+            strokeWidth="4" // Đặt độ dày của viền.
+            strokeLinecap="round" //Đặt kiểu kết thúc của đường viền là tròn.
+            strokeLinejoin="round" // Đặt kiểu kết thúc của đường viền là tròn.
+            initial={{ strokeDasharray: "24 10 0 0" }}
+            animate={{
+              strokeDasharray: ["15 120 25 25", "16 25 92 72", "4 250 22 22"],
+              rotate: [120, 360],
+            }} // Trạng thái chuyển động của hình tròn
+            transition={{
+              duration: 20,
+              repeat: Infinity,
+              repeatType: "reverse",
+            }}
+          />
+        </motion.svg>
+      </motion.div>
+    </div>
+  );
 };
 
 export default Photo;
